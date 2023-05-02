c3a6aa26f547060a1a42ac4c178ea8dfc6ae0305
Stephen-Onochie <stephenconochie@gmail.com>
2023-05-02 18:10:59 -0400

    More practice on Drawing

--- draw.py
+++ draw.py
@@ -7,11 +7,15 @@ import cv2 as cv
 import numpy as np
                 #width, height, & # of color channels
 blank = np.zeros((500,500, 3), dtype='uint8') #uint8 is the datatype for an image
-cv.imshow('Blank', blank)
+# cv.imshow('Blank', blank)
 
     #1. Paint the image a certain color
-blank[:] = 0,255,0
-cv.imshow('Green', blank)
+# blank[200:300, 300:400] = 0,255,0
+# cv.imshow('Green', blank)
+
+#Draw a rectangle (Bottom Left Point, Top Right Point, & thickness)
+cv.rectangle(blank, (0,0), (150,250), (0,255,0), thickness=2)
+cv.imshow('Rectangle', blank)
 
 # img = cv.imread('Photos/dog1.jpg')
 # cv.imshow('Dog', img)
