53d4b99d05c99516983f114ce24e95a1a305eed1
Stephen O <stephenconochie@gmail.com>
2023-06-23 23:09:54 -0400

    Started work on creating detector class

--- HandSignDetection/AtoI.py
+++ HandSignDetection/AtoI.py
@@ -9,67 +9,76 @@ from cvzone.ClassificationModule import Classifier
 import numpy as np
 import time
 
-cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
-classifier = Classifier("Models/AtoI/keras_model.h5", "Models/AtoI/labels.txt")
 
-offset = 20
-imgSize = 300
+class detector:
+    def __init__(self, model_file_path, label_file_path):
+        #TODO turn detection progrom into a class
+        #TODO class should have init, detect, & close methods
 
-#folder = "Data/C"
-counter = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector(maxHands=1)
+    classifier = Classifier("Models/AtoI/keras_model.h5", "Models/AtoI/labels.txt")
 
-labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+    offset = 20
+    imgSize = 300
 
-while True:
-    success, img = cap.read()
-    imgOutput = img.copy()
-    hands, img = detector.findHands(img)
-    if hands:
-        hand = hands[0]
-        x, y, w, h = hand["bbox"]
+    # folder = "Data/C"
+    counter = 0
 
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
-        imgCrop = img[y-offset:y + h + offset, x - offset:x + w + offset]
+    labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
-        imgCropShape = imgCrop.shape
+    while True:
+        success, img = cap.read()
+        imgOutput = img.copy()
+        hands, img = detector.findHands(img)
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand["bbox"]
 
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
-        aspectRatio = h / w
+            imgCropShape = imgCrop.shape
 
-        if aspectRatio > 1:
-            k = imgSize / h
-            wCal = math.ceil(k*w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            imgResizeShape = imgResize.shape
-            wGap = math.ceil((imgSize - wCal) / 2)
-            imgWhite[:, wGap:wCal + wGap] = imgResize
-            prediction, index = classifier.getPrediction(imgWhite, draw=False)
-            print(prediction, index)
+            aspectRatio = h / w
 
-        else:
-            k = imgSize / w
-            hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            imgResizeShape = imgResize.shape
-            hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap:hCal + hGap, :] = imgResize
-            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            if aspectRatio > 1:
+                k = imgSize / h
+                wCal = math.ceil(k * w)
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                imgResizeShape = imgResize.shape
+                wGap = math.ceil((imgSize - wCal) / 2)
+                imgWhite[:, wGap:wCal + wGap] = imgResize
+                prediction, index = classifier.getPrediction(imgWhite, draw=False)
+                print(prediction, index)
 
-        cv2.rectangle(imgOutput, (x-offset,y-offset - 50),
-                      (x - offset + 90, y-offset - 50+50), (255,0,255), cv2.FILLED)
-        cv2.putText(imgOutput, labels[index], (x, y-30), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
-        cv2.rectangle(imgOutput, (x-offset,y-offset), (x+w+offset, y+h+offset), (255,0,255), 4)
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize
+                prediction, index = classifier.getPrediction(imgWhite, draw=False)
 
-        # cropped image
-        # cv2.imshow("ImageCrop", imgCrop)
+            cv2.rectangle(imgOutput, (x - offset, y - offset - 50),
+                          (x - offset + 90, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
+            cv2.putText(imgOutput, labels[index], (x, y - 30), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
 
-        # image w/ white background
-        # cv2.imshow("ImageWhite", imgWhite)
+            # predicted value
+            print(labels[index])
 
-    # main image
-    cv2.imshow("Image", imgOutput)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+            cv2.rectangle(imgOutput, (x - offset, y - offset), (x + w + offset, y + h + offset), (255, 0, 255), 4)
+
+            # cropped image
+            # cv2.imshow("ImageCrop", imgCrop)
+
+            # image w/ white background
+            # cv2.imshow("ImageWhite", imgWhite)
+
+        # main image
+        cv2.imshow("Image", imgOutput)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
